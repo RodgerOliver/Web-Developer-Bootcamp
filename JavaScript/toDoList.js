82ae5bbd77fc90ce421199888188fc56a8ec94f9
RodgerOliver <timeline.mb@gmail.com>
2017-11-10 16:07:37 -0200

    added dlete option to todo list and changed some other things

--- JavaScript/toDoList.js
+++ JavaScript/toDoList.js
@@ -3,12 +3,23 @@ var todos = [];
 
 while(input !== "quit" || input === false) {
 	if(input === "list") {
-		console.log(todos);
+		console.log("********************");
+		todos.forEach(function(todo, index) {
+			console.log(index + ": " + todo);
+		})
+		console.log("********************");
 	}
 
 	else if(input === "new") {
 		var newTodo = prompt("Enter a new todo");
+		console.log("Added todo");
 		todos.push(newTodo);
+	}
+
+	else if(input === "delete") {
+		var index = prompt("Enter index of todo to delete");
+		todos.splice(index, 1);
+		console.log("Deleted todo");
 	}
 
 	input = prompt("What would you like to do?");
