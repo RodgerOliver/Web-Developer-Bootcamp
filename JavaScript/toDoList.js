4625c4075719c602c67e490989fe2ce7f2cb551e
RodgerOliver <timeline.mb@gmail.com>
2017-11-10 16:15:25 -0200

    added functions

--- JavaScript/toDoList.js
+++ JavaScript/toDoList.js
@@ -3,25 +3,38 @@ var todos = [];
 
 while(input !== "quit" || input === false) {
 	if(input === "list") {
-		console.log("********************");
-		todos.forEach(function(todo, index) {
-			console.log(index + ": " + todo);
-		})
-		console.log("********************");
+		listTodo();
 	}
 
 	else if(input === "new") {
-		var newTodo = prompt("Enter a new todo");
-		console.log("Added todo");
-		todos.push(newTodo);
+		addTodo();
 	}
 
 	else if(input === "delete") {
-		var index = prompt("Enter index of todo to delete");
-		todos.splice(index, 1);
-		console.log("Deleted todo");
+		deleteTodo();
 	}
 
 	input = prompt("What would you like to do?");
 }
 alert("Thanks for using this Todo List!");
+
+
+function listTodo() {
+	console.log("********************");
+		todos.forEach(function(todo, index) {
+			console.log(index + ": " + todo);
+		})
+		console.log("********************");
+}
+
+function addTodo() {
+	var newTodo = prompt("Enter a new todo");
+		console.log("Added todo");
+		todos.push(newTodo);
+}
+
+function deleteTodo() {
+	var index = prompt("Enter index of todo to delete");
+		todos.splice(index, 1);
+		console.log("Deleted todo");
+}
