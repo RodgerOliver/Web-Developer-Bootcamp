3a68fb0c79dd6a75812fdaf048bcdfd217a5fac7
RodgerOliver <timeline.mb@gmail.com>
2017-11-14 18:29:23 -0200

    color game #4

--- Color Game/colorGame.js
+++ Color Game/colorGame.js
@@ -1,15 +1,9 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
-]
-
+var colors = pushColors(6);
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.querySelector("#colorDisplay");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
+var msg = document.querySelector('#msg');
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -19,10 +13,40 @@ for(i=0; i<squares.length; i++) {
 	squares[i].addEventListener("click", function() {
 		var clickedColor = this.style.backgroundColor;
 		if(clickedColor === pickedColor) {
-			alert("correct");
+			msg.textContent = "Correct!";
+			changeColor(pickedColor);
+			h1.style.backgroundColor = clickedColor;
 		}
 		else {
-			alert("wrong");
+			this.style.backgroundColor = "#232323";
+			msg.textContent = "Try Again!";
 		}
 	})
+}
+
+function changeColor(color) {
+	for(i=0; i<squares.length; i++) {
+		squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function pushColors(num) {
+	var arr = [];
+	for(i=0; i<num; i++) {
+		arr.push(generateColors());
+	}
+	return arr;
+}
+
+function generateColors() {
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+	var rgb = "rgb(" + r + ", " + b + ", " + g + ")";
+	return rgb;
 }
