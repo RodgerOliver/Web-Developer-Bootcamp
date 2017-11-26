ea53a20e24e95b9039ec740514951dc2cf264c8e
RodgerOliver <timeline.mb@gmail.com>
2017-11-26 13:49:28 -0200

    learning Node, NPM and their packages

---
+++ introNPM/echo.js
@@ -0,0 +1,8 @@
+function echo(msg, numb) {
+    for(var i=0; i<numb; i++) {
+        console.log(msg);
+    }
+}
+
+echo("Rodger is learning with Colt Steele", 5);
+echo("Colt Steele is teaching Rodger", 2);
---
+++ introNPM/grader.js
@@ -0,0 +1,17 @@
+function average(arr) {
+    
+    var sum = 0;
+    for(var i=0; i<arr.length; i++) {
+        sum += arr[i];
+    }
+    var avg = sum/arr.length;
+    return (Math.round(avg));
+}
+
+console.log("• Average score for environment science");
+var scores = [90, 98, 89, 100, 100, 86, 94];
+console.log("====== "+average(scores)+" ======");
+
+console.log("• Average score for organic chemistry");
+var scores2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
+console.log("====== "+average(scores2)+" ======");
