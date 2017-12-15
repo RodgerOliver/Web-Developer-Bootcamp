45d8ee129077af020906c0274ec388089f73cd73
RodgerOliver <timeline.mb@gmail.com>
2017-12-15 13:03:32 -0200

    learning about routes

--- introExpress/firstExpressApp/app.js
+++ introExpress/firstExpressApp/app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 
+
 app.get("/", function(req, res) {
 	res.send("Hi There!");
 });
@@ -10,8 +11,22 @@ app.get("/bye", function(req, res) {
 	res.send("Bye guys!");
 });
 
+app.get("/r/:subredditName", function(req, res) {
+	console.log(req.params);
+	var subreddit = req.params.subredditName;
+	res.send("WELCOME TO THE " + subreddit.toUpperCase() + " SUBREDDIT!!");
+});
 
+app.get("/r/:subredditName/comments/:id/:title", function(req, res) {
+	res.send("welcome to the comments page");
+});
 
+//when the route doesn't exists use "*" to send the page  
+app.get("*", function(req, res) {
+	res.send("This page doesn't exists");
+});
+
+// what port the server is running
 app.listen(3000, function() {
 	console.log("working on port 3000");
 });
