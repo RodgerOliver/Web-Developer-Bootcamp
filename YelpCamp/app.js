d6d3a5fabf0d8667bfa6190de381988bea9e6f43
RodgerOliver <timeline.mb@gmail.com>
2017-12-24 17:21:55 -0200

    YelpCamp renamed files, created the route SHOW RESTful route, added description to the camps'

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -7,13 +7,15 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+// mongoose setup
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useMongoClient: true});
 
 // schema and model setup
 var campSchema = new mongoose.Schema({
 	name: String,
-	image: String
+	image: String,
+	description: String
 });
 var Camp = mongoose.model("Camp", campSchema);
 
@@ -22,35 +24,55 @@ app.get("/", function(req, res) {
 	res.render("home");
 });
 
+// INDEX - show all camps
 app.get("/camps", function(req, res) {
 	// get all the campgrounds
 	Camp.find({}, function(err, camps) {
 		if(err) {
 			console.log(err);
 		} else {
-			res.render("camps", {camps: camps});
+			res.render("index", {camps: camps});
 		}
 	});
 
 });
 
+// CREATE - add new camp to DB
 app.post("/camps", function(req, res) {
 	var name = req.body.name;
 	var url = req.body.url;
-	var newCamp = {name: name, image: url};
+	var description = req.body.description;
+	var newCamp = {name: name, image: url, description: description};
 	// create and add a new campground
 	Camp.create(newCamp, function(err, newCamp) {
 		if(err) {
 			console.log(err);
 		} else {
-			res.redirect("/camps");	
+			res.redirect("/camps");
 		}
 	});
 });
 
+// NEW - show form to create a new camp
 app.get("/camps/new", function(req, res) {
-	res.render("newCamp");
+	res.render("new");
 });
+
+// SHOW - show more info about one camp
+app.get("/camps/:id", function(req, res) {
+	// find the camp with provide ID
+	var id = req.params.id;
+	Camp.findById(id, function(err, campById) {
+		if(err) {
+			console.log(err);
+		} else {
+			// show template with that camp
+			res.render("show", {
+				camp: campById
+			});
+		}
+	})
+})
 
 app.listen(3000, function() {
 	console.log("Working on port 3000!");
