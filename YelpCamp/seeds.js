847fa7f1a30f27e8998b0ec4c80d374cc83a0d39
RodgerOliver <timeline.mb@gmail.com>
2017-12-28 19:52:08 -0200

    YelpCamp Seeds styled the app, except for the home

--- YelpCamp/seeds.js
+++ YelpCamp/seeds.js
@@ -6,7 +6,7 @@ var data = [
 	{
 		name: "Cloud's Rest",
 		image: "http://haulihuvila.com/wp-content/uploads/2012/09/hauli-huvila-campgrounds-lg.jpg",
-		description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+		description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 	},
 	{
 		name: "Bacon Camp",
